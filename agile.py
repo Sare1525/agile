5c5d9f95ca69ec2bbb7d98cbb1dc9cdaa5b12ba2
Sare1525 <santhosh.2023c@vitstudent.ac.in>
2026-01-08 19:48:02 +0530

    added feild to add author and print it

--- agile.py
+++ agile.py
@@ -1,8 +1,10 @@
 n=int(input("enter the number of books:"))
 books=[]
+author=[]
 for i in range(n):
     books.append(input("enter the book name: "))
-print("Book")
+    author.append(input("enter the author name: "))
+print("Book",'   ',"Author")
 for i in range(n):
-    print(books[i],end="\n")
+    print(books[i],"  ",author[i],end="\n")
 print("Thank you")
