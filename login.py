ff029d06f94d3e6765103adf26f260033951b417
Sare1525 <santhosh.2023c@vitstudent.ac.in>
2026-01-08 19:54:13 +0530

    added login file

---
+++ login.py
@@ -0,0 +1,8 @@
+name="admin"
+password="1234"
+name1=input("Enter your name:")
+password1=input("Enter your password:")
+if name1==name and password1==password:
+    print("Login successful")
+else:
+    print("Login failed")
